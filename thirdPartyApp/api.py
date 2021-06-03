80c72492bfd75892fab527d69cee50e054e0097c
RezaKeshtkarzadeh <reza.rmb.ajmh@gmail.com>
2021-06-04 00:43:51 +0430

    Update api.py
    update User class
    remake and improve customer api
    
    Co-Authored-By: Alavi Isaac <83088307+alavi-isaac@users.noreply.github.com>
    Co-Authored-By: rezanaim <58845986+rezanaim@users.noreply.github.com>

--- thirdPartyApp/api.py
+++ thirdPartyApp/api.py
@@ -1,3 +1,5 @@
+""" Main Class """ #===============================================================
+
 class User:
     def __init__(self, fName, lName, phone, email, password):
         self.__first_name = fName
@@ -48,3 +50,85 @@ class User:
 
     def __str__(self) -> str:
         return self.__first_name + " " + self.__last_name
+
+
+# ======================================================================
+""" Customer Inheritance class """ # ===================================
+
+class customer(User):
+    def __init__(self, id_customer, username, card, basket, history, comments):
+        self.__id = id_customer
+        self.__username = username
+        self.__card = card
+        self.__basket = basket
+        self.__history = history
+        self.__comments = comments
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, value):
+        self.__id = value
+
+    @property
+    def uname(self):
+        return self.__username
+    
+    @uname.setter
+    def uname(self, value):
+        self.__username = value
+
+    @property
+    def card(self):
+        return self.__card
+
+    @card.setter
+    def card(self, value):
+        self.__card = value
+
+    @property
+    def basket(self):
+        return self.__basket
+
+    @basket.setter
+    def basket(self, value):
+        self.__basket = value
+
+    @property
+    def history(self):
+        return self.__history
+
+    @history.setter
+    def history(self, value):
+        self.__history = value
+
+    @property
+    def comments(self):
+        return self.__comments
+    
+    @comments.setter
+    def comments(self, value):
+        self.__comments = value
+
+    def __str__(self):
+        return self.__username + "( " + self.__first_name + " " + self.__last_name + " )"
+
+# ================================================================
+""" Seller Inheritance class """ # ===============================
+
+
+
+
+
+
+# ================================================================
+""" stuff Inheritance class """ # ================================
+
+
+
+
+
+
+# =================================================================
