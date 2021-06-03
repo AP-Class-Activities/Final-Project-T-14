d361bf4713f0f10a0330840b666820c2238949cb
Alavi Isaac <83088307+Alavi-Isaac@users.noreply.github.com>
2021-06-03 09:53:47 -0700

    c2
    c2
    
    Co-Authored-By: RezaKeshtkarzadeh <83055785+RMBsck2000@users.noreply.github.com>
    Co-Authored-By: rezanaim <58845986+rezanaim@users.noreply.github.com>

--- thirdPartyApp/api.py
+++ thirdPartyApp/api.py
@@ -1,16 +1,19 @@
 class Person:
-    def __init__(self, name, email):
+    def __init__(self, name, email, password):
         self.__name = name
         self.__email = email
+        self.__password = password 
 
     def id(self):
         pass
 class customer(Person):
-    def __init__(self,card,basket,username):
+    def __init__(self,card,basket,username,id_customer,comments,history):
         self.card = card
         self.basket = basket
-
-
+        self.username = username
+        self.id_customer = id_customer
+        self.history = history
+        self.comments = comments
 
 class new:
     def __init__(self, name):
