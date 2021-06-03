02617b38450023de37d9fef071111470bc893a85
RezaKeshtkarzadeh <reza.rmb.ajmh@gmail.com>
2021-06-04 00:19:08 +0430

    Update api.py
    user's objects have been made.
    
    Co-Authored-By: Alavi Isaac <83088307+alavi-isaac@users.noreply.github.com>
    Co-Authored-By: rezanaim <58845986+rezanaim@users.noreply.github.com>

--- thirdPartyApp/api.py
+++ thirdPartyApp/api.py
@@ -1,25 +1,50 @@
-class Person:
-    def __init__(self, name, email, password):
-        self.__name = name
+class User:
+    def __init__(self, fName, lName, phone, email, password):
+        self.__first_name = fName
+        self.__last_name = lName
+        self.__phone_numer = phone
         self.__email = email
-        self.__password = password 
+        self.__password = password
 
-    def id(self):
-        pass
-class customer(Person):
-    def __init__(self,card,basket,username,id_customer,comments,history):
-        self.card = card
-        self.basket = basket
-        self.username = username
-        self.id_customer = id_customer
-        self.history = history
-        self.comments = comments
+    @property
+    def fName(self):
+        return self.__first_name
 
-class new:
-    def __init__(self, name):
-        self.__name = name
+    @fName.setter
+    def fName(self,value): 
+        self.__first_name = value
 
+    @property
+    def lName(self):
+        return self.__last_name
 
-class RMB:
-    def __init__(self) -> None:
-        pass
+    @lName.setter
+    def lName(self, value):
+        self.__last_name = value
+
+    @property
+    def phone(self):
+        return self.__phone_numer
+
+    @phone.setter
+    def phone(self, value):
+        self.__phone_numer = value
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        self.__email = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        self.__password = value
+
+    def __str__(self) -> str:
+        return self.__first_name + " " + self.__last_name
