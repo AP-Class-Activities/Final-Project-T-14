58a5039fa177818b488347e3ce110cfe1b78ef45
RezaKeshtkarzadeh <reza.rmb.ajmh@gmail.com>
2021-06-03 18:49:30 +0430

    Update api.py
    Co-Authored-By: Alavi Isaac <83088307+alavi-isaac@users.noreply.github.com>

--- thirdPartyApp/api.py
+++ thirdPartyApp/api.py
@@ -1,3 +1,13 @@
 class Person:
-    def __init__(self, username, fName, Lname, email, password, phone, address):
-        pass
+    def __init__(self, username, fName, lName, email, password, phone, address):
+        self.__username = username
+        self.__first_name = fName
+        self.__last_name = lName
+        self.__email = email
+        self.__password = password
+        self.__phone = phone
+        self.__address = address
+
+    @property
+    def uName(self):
+        return self.__username
