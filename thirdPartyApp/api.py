243f8545056c6aa06dd17df02d0c9cbfe20b03dd
RezaKeshtkarzadeh <reza.rmb.ajmh@gmail.com>
2021-06-08 16:11:44 +0430

    Update and Improve APIs and code!
    Co-Authored-By: Alavi Isaac <83088307+Alavi-Isaac@users.noreply.github.com>
    Co-Authored-By: rezanaim <58845986+rezanaim@users.noreply.github.com>

--- thirdPartyApp/api.py
+++ thirdPartyApp/api.py
@@ -62,7 +62,7 @@ class customer(User):
         self.__card = card
         self.__basket = basket
         self.__history = history
-        self.__distance = distance
+        self.__distance = distance  # """ fasel anbar ta moshtari """
 
     @property
     def id(self):
@@ -86,7 +86,10 @@ class customer(User):
 
     @card.setter
     def card(self, value):
-        self.__card = value
+        if value < 0:
+            ValueError("")
+        else:
+            self.__card = value
 
     @property
     def basket(self):
@@ -126,20 +129,17 @@ class customer(User):
 # ================================================================
 """ Seller Inheritance class """ # ===============================
 
-class customer(User):
-    def __init__(self, id_customer, username, card, basket, history, comments):
-        self.__id = id_customer
-        self.__username = username
-        self.__card = card
-        self.__basket = basket
-        self.__history = history
-        self.__comments = comments
-	def buy(self,stuff,price):
-        	if self.card < price:
-        		print('first charge your card')
-        	else:
-         		self.card-=price
-         		history+=stuff
+class seller(User):
+    def __init__(self, id_seller, address, offer_list, income, sell_counter, cost, distance, suspension):
+        self.__id = id_seller
+        self.__address = address
+        self.__offer_list = offer_list
+        self.__income = income
+        self.__sell_counter = sell_counter
+        self.__cost = cost
+        self.__distance = distance  # """ fasel foroshande ta anbar """
+        self.__suspension = suspension
+        
     @property
     def id(self):
         return self.__id
@@ -147,149 +147,74 @@ class customer(User):
     @id.setter
     def id(self, value):
         self.__id = value
+        
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, value):
+        self.__address = value
 
     @property
-    def uname(self):
-        return self.__username
+    def offer_list(self):
+        return self.__offer_list
+
+    @offer_list.setter
+    def offer_list(self, value):
+        self.__offer_list = value
+
+    @property
+    def income(self):
+        return self.__income
+
+    @income.setter
+    def income(self, value):
+        self.__income = value
+
+    @property
+    def sell_counter(self):
+        return self.__sell_counter
+
+    @sell_counter.setter
+    def sell_counter(self, value):
+        if value < 0:
+            print("sell_counter should be positive")
+        else:
+            self.__sell_counter = value
     
-    @uname.setter
-    def uname(self, value):
-        self.__username = value
+    @property
+    def cost(self):
+        return self.__cost
+
+    @cost.setter
+    def cost(self, value):
+        if value < 0:
+            print("cost should be positive")
+        else:
+            self.__cost = value
 
     @property
-    def card(self):
-        return self.__card
+    def distance(self):
+        return self.__distance
 
-    @card.setter
-    def card(self, value):
-        self.__card = value
-
-    @property
-    def basket(self):
-        return self.__basket
-
-    @basket.setter
-    def basket(self, value):
-        self.__basket = value
+    @distance.setter
+    def distance(self, value):
+        self.__distance = value
 
     @property
-    def history(self):
-        return self.__history
+    def suspension(self):
+        return self.__suspension
 
-    @history.setter
-    def history(self, value):
-        self.__history = value
-
-    @property
-    def comments(self):
-        return self.__comments
+    @suspension.setter
+    def suspension(self, value):
+        if value is not True or False:
+            print("suspension should be True or False")
+        else:
+            self.__suspension = value
     
-    @comments.setter
-    def comments(self, value):
-        self.__comments = value
-
     def __str__(self):
-        return self.__username + "( " + self.__first_name + " " + self.__last_name + " )"
-
-# ================================================================
-""" Seller Inheritance class """ # ===============================
-
-
-class seller(User):
-    def __init__(self, fName= "first name", lName= "last_name", phone, email, password,     id, address= "tehran", offer_list=[], income=0 , sell_counter=0, cost=0, suspension=False):
-        super(seller, self).__init__(fName, lName, phone, email, password)
-        self.__id= id
-        self.__address= address
-        self.__offer_list= offer_list
-        self.__income= income
-        self.__sell_counter= sell_counter
-        self.__cost= cost
-        self.__suspension= suspension
-        
-        
-        def selling(self):
-            return self.income += self.cost and self.sell_counter += 1
-        
-        
-        
-        @property
-        def id(self):
-            return self.__id
-
-        @id.setter
-        def id(self, value):
-            self.__id = value
-            
-        
-        @property
-        def address(self):
-            return self.__address
-            
-        @address.setter
-        def address(self, value):
-            self.__address = value
-
-        
-        @property
-        def offer_list(self):
-            return self.__offer_list
-
-        @offer_list.setter
-        def offer_list(self, value):
-            return self.__offer_list
-
-        
-        @property
-        def income(self):
-            return self.__income
-
-        @income.setter
-        def income(self, value):
-            return self.__income
-
-        
-        @property
-        def sell_counter(self):
-            return self.__sell_counter
-
-        @sell_counter.setter
-        def sell_counter(self, value):
-            if self.sell_counter < 0:
-                print("sell_counter should be positive")
-            else:
-                return self.__sell_counter
-
-        
-        @property
-        def cost(self):
-            return self.__cost
-
-        @cost.setter
-        def cost(self, value):
-            if self.cost < 0:
-                print("cost should be positive")
-            else:
-                return self.__cost
-
-
-        @property
-        def suspension(self):
-            return self.__suspension
-
-        @suspension.setter
-        def suspension(self, value):
-            if self.suspension is True or False:
-                return self.__suspension
-            else:
-                print("suspension should be True or False")
-        
-
-            
-                
-        def __str__(self):
-            return "seller--> (%s)"%(super(seller,self).__str__())
-
-
+        return "seller--> (%s)"%(super(seller,self).__str__())
 
 # ================================================================
 """ stuff Inheritance class """ # ================================
@@ -326,8 +251,21 @@ class stuff:
     def name(self, value):
         self.__name = value
 
+    @property
+    def price(self):
+        return self.__price
 
+    @price.setter
+    def price(self, value):
+        self.__price = value
 
+    @property
+    def specification(self):
+        return self.__specification
+
+    @specification.setter
+    def specification(self, value):
+        self.__specification = value
 
 # =================================================================
 
